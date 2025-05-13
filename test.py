c6a01b79c4d0f88dffddfff37a57548beadf3c9f
jihoo <141470272+jihoof@users.noreply.github.com>
2025-05-13 21:01:21 +0800

    update

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-def minmax(list):
-    max1 = max(list)
-    min1 = min(list)
-    list.remove(max(list))
-    list.remove(min(list))
-    return max1, min1, list
+def coin(money):
+    fh = money // 500
+    oh = money % 500 // 100
+    fih = money % 500 % 100 // 50
+    th = money % 500 % 100 % 50 //10
+    return fh, oh, fih, th
 
-l = [0,1,2,5]
-
-print(minmax(l))
+money = int(input())
+print(coin(money))
