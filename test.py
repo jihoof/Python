75fa32c0590f93afea1e28037190149f9313eba8
jihoo <141470272+jihoof@users.noreply.github.com>
2025-08-06 21:51:46 +0900

    d

--- test.py
+++ test.py
@@ -1,5 +1,39 @@
-# tmp = 10
-# for _ in range(10):
-#     print(tmp, end= ' ')
-#     tmp -= 1
-# print("happy new year")
+stair_number = int(input())
+all_list = [[] for i in range(stair_number+2)]
+for _ in range(stair_number+1):
+    all_list[0].append(" ")
+comp_move = 4
+player_move = 7
+
+if stair_number % 2 != 0:
+    for i in range(1, ((stair_number+1)//2) + 1):
+        for _ in range(i):
+            all_list[i].append('■')
+        for _ in range((stair_number+1)-(i*2)):
+            all_list[i].append(" ")
+        for _ in range(i):
+            all_list[i].append('■')
+   
+elif stair_number % 2 == 0:
+    for i in range(1, (stair_number//2) + 1):
+        for _ in range(i):
+            all_list[i].append('■')
+        for _ in range((stair_number+1)-(i*2)):
+            all_list[i].append(" ")
+        for _ in range(i):
+            all_list[i].append('■')
+
+if player_move <= (stair_number-1) // 2:
+    all_list[player_move][player_move] =  "○"
+elif player_move > (stair_number-1) // 2:
+    all_list[stair_number-player_move][player_move] = "○"
+if comp_move >(stair_number-1) // 2:
+    all_list[stair_number-comp_move][-comp_move-1] =  "●"
+elif comp_move <= (stair_number-1) // 2:
+    all_list[comp_move][-comp_move-1] = "●"
+
+
+for line in all_list:
+    for i in line:
+        print(i,end = '')
+    print()
