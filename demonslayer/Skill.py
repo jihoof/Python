a5de4a8a873ac0076984d99f2c99acf169dd9c6b
yojun313 <moonyojun@gmail.com>
2025-11-27 20:50:49 +0900

    Refactor character and skill classes; remove unused files and implement game dictionary for skill details.

---
+++ demonslayer/Skill.py
@@ -0,0 +1,60 @@
+from load import game_dict
+
+class Skill:
+    def __init__(self, skill_name):
+        self.skill_name = skill_name
+        
+        self.detail = game_dict[skill_name]["detail"]
+        self.selfEffect = game_dict[skill_name]["selfEffect"]
+        
+        self.enemyStatus = game_dict[skill_name]["enemyStatus"]
+        self.enemyDamage = game_dict[skill_name]["enemyDamage"]
+        
+        # Enemy status & damage stats
+        self.BreathMultiplier = self.enemyDamage['BreathMultiplier']
+        self.MartialMultiplier = self.enemyDamage['MartialMultiplier']
+        self.RegenerationInhibitionLevel = self.enemyStatus['RegenerationInhibitionLevel']
+        self.BrandOfTheSunLevel = self.enemyStatus['BrandOfTheSunLevel']
+    
+    def status_use(self, player, enemy):
+        for field, stat_str in self.selfEffect.items():
+            effect_str, turn = stat_str[0], stat_str[1]      
+            player.stat[field] = self.change_stat(player.stat[field], effect_str)
+            player.passive_list.append([field, turn, effect_str])  
+            
+        for field, stat_str in self.enemyEffect.items():
+            effect_str, turn = stat_str[0], stat_str[1]      
+            enemy.stat[field] = self.change_stat(player.stat[field], effect_str)
+            enemy.passive_list.append([field, turn, effect_str])  
+    
+    def change_stat(self, stat_str, player_stat):
+        if type(stat_str) == bool:
+            return stat_str
+
+        elif 'x' in stat_str:
+            multiplier = float(stat_str.split('_')[1])
+            return player_stat * multiplier
+
+        elif '+' in stat_str:
+            addition = float(stat_str.split('_')[1])
+            return player_stat + addition
+
+        elif '-' in stat_str:
+            subtraction = float(stat_str.split('_')[1])
+            return player_stat - subtraction
+        
+    def original_stat(self, player_stat, stat_str):
+        if type(stat_str) == bool:
+            return not stat_str
+        
+        elif 'x' in stat_str:
+            multiplier = float(stat_str.split('_')[1])
+            return player_stat / multiplier
+        
+        elif '+' in stat_str:
+            addition = float(stat_str.split('_')[1])
+            return player_stat - addition
+        
+        elif '-' in stat_str:
+            subtraction = float(stat_str.split('_')[1])
+            return player_stat + subtraction
