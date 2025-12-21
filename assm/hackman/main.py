47b64f1c7534e4fcb5844a23af3cb8c9ca1b8b75
yojun313 <moonyojun@gmail.com>
2025-12-21 20:21:53 +0900

    done

--- assm/hackman/main.py
+++ assm/hackman/main.py
@@ -1,15 +1,64 @@
 import setting
 import random 
 import module
-import os
-import sys
 import getpass
-import time
-import rich
 
 class Hackman:
     def __init__(self):
+        self.nickname = None
         
+        while True:
+            module.clear()
+            print('Hang Man game.inc')
+            print("관리자 로그인을 하시려면 '1'을, 로그인 하시려면 '2'를, 계정을 생성하려면 '3'을 입력해주세요.")
+            select = module.input_int(1,3,'입력: ','잘못된 입력입니다.')
+
+            if select == 1:
+                admin = self.admin_login()
+                if admin == True:
+                    module.clear()
+                
+                    while True:
+                        select = module.input_int(1,2,'관리자 메뉴를 열람할려면 1번, 로그아웃하기 위해선 2번을 입력해주새요: ', '비밀번호가 일치하지 않습니다.')
+                        if select == 1:
+                            self.print_user_list()
+                            while True:
+                                select = module.input_int(1,2,'유저를 삭제하시려면 1번, 종료하려면 2번을 입력해주세요: ', '잘못된 입력입니다.')
+                                if select == 1:
+                                    print()
+                                    selected_user = input('삭제할 유저의 닉네임을 입력해주세요: ')
+                                    if setting.players.find_one({'nickname' : selected_user}):
+                                        setting.players.delete_one({'nickname' : selected_user})
+                                        print('유저가 정상적으로 삭제되었습니다.')
+                                        module.enter()
+                                        continue
+                                    else:
+                                        print('해당 유저가 존재하지 않습니다.')
+                                        module.enter()
+                                        continue 
+                                else:
+                                    break
+                        else:
+                            print('로그아웃 합니다.')
+                            break
+
+
+                else:
+                    print('비밀번호가 일치하지 않습니다.')
+                    print('시작화면으로 되돌아갑니다.')
+                    continue
+
+            elif select == 2:
+                nickname = self.sign_in()
+                if nickname:
+                    print('로그인 되셨습니다.')
+                    self.nickname = nickname
+                    module.enter()
+                    break
+            
+            elif select == 3:
+                self.sign_up()
+                
     def sign_up(self):
 
         nickname = input('아이디를 입력하세요: ')
@@ -30,16 +79,14 @@ class Hackman:
 
             })
 
-    def admin_login():
+    def sign_in(self):
+        return "nickname"
+    
+    def admin_login(self):
 
         password = getpass.getpass('관리자 비밀번호 입력하세요: ')
 
-        admin = setting.players.find_one({
-
-            'nickname' : 'admin'
-
-        })
-
+        admin = setting.players.find_one({'nickname' : 'admin'})
         admin_password = admin['password']
 
         if password == admin_password:
@@ -50,9 +97,7 @@ class Hackman:
             print('비밀번호가 잘못되었습니다.')
             return False
             
-            
-
-    def reveal_word(comp_word, used):
+    def reveal_word(self, comp_word, used):
         for i in comp_word:
             if i not in used:
                 print("_", end = " ")
@@ -60,16 +105,16 @@ class Hackman:
                 print(i, end = " ")
         return ""
 
-    def print_status(comp_word, used, life):
+    def print_status(self, comp_word, used, life):
         print("---------------------------------------------")
         print(f"Word: ", end = ' ')
-        reveal_word(comp_word, used)
+        self.reveal_word(comp_word, used)
         print()
         print(f"Used: {' '.join(used)}")
         print(f"Life: {life}")
         print("---------------------------------------------")
 
-    def is_word_guessed(comp_word, used):
+    def is_word_guessed(self, comp_word, used):
         tmp = list(comp_word)
         tmp2 = len(tmp)
         tmp3 = 0
@@ -81,14 +126,11 @@ class Hackman:
         else:
             return False
         
-    def print_user_list():
+    def print_user_list(self):
         user_list = list(setting.players.find({},{'_id' : 0, 'password' : 0}))
         module.beautiful_table(user_list, title = 'User List', show_index = True)
 
-    def login():
-        pass
-
-    def main():
+    def main(self):
         play_again = "none"
 
         intel_setup = True
@@ -104,68 +146,8 @@ class Hackman:
             else:
                 pass
             
-            while True:
-                module.clear()
-                print('Hang Man game.inc')
-                print("관리자 로그인을 하시려면 '1'을, 로그인 하시려면 '2'를, 계정을 생성하려면 '3'을 입력해주세요.")
-                select = module.input_int(1,3,'입력: ','잘못된 입력입니다.')
-
-                if select == 1:
-                    admin = admin_login()
-                    if admin == True:
-                        module.clear()
-                    
-                        while True:
-                            select = module.input_int(1,2,'관리자 메뉴를 열람할려면 1번, 로그아웃하기 위해선 2번을 입력해주새요: ', '비밀번호가 일치하지 않습니다.')
-                            if select == 1:
-                                print_user_list()
-                                while True:
-                                    select = module.input_int(1,2,'유저를 삭제하시려면 1번, 종료하려면 2번을 입력해주세요: ', '잘못된 입력입니다.')
-                                    if select == 1:
-                                        print()
-                                        selected_user = input('삭제할 유저의 닉네임을 입력해주세요: ')
-                                        if setting.players.find_one({
-
-                                            'nickname' : selected_user
-                                        }):
-                                            setting.players.delete_one({
-
-                                                'nickname' : selected_user
-
-                                                })
-                                            print('유저가 정상적으로 삭제되었습니다.')
-                                            module.enter()
-                                            continue
-                                        else:
-                                            print('해당 유저가 존재하지 않습니다.')
-                                            module.enter()
-                                            continue 
-                                    else:
-                                    break
-                            else:
-                                print('로그아웃 합니다.')
-                                break
-
-
-                    else:
-                        print('비밀번호가 일치하지 않습니다.')
-                        print('시작화면으로 되돌아갑니다.')
-                        continue
-
-                elif select == 2:
-                    logedin = login()
-                    if logedin == True:
-                        print('로그인 되셨습니다.')
-                        module.enter()
-                        break
-                
-                elif select == 3:
-                    pass
-
-
-
-            print_status(comp_word, USED, LIFE)
-            word_guessed = is_word_guessed(comp_word, USED)
+            self.print_status(comp_word, USED, LIFE)
+            word_guessed = self.is_word_guessed(comp_word, USED)
 
             if LIFE == 0:
                 print(f"The answer was {comp_word}. ")
@@ -198,5 +180,6 @@ class Hackman:
             elif play_again == "no":
                 break
 
-main()
+game = Hackman()
+game.main()
 
