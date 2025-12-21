08fd8efe1777440a08ca0f5529c1b4d969f18122
jihoo <141470272+jihoof@users.noreply.github.com>
2025-12-21 19:14:10 +0800

    선생님 코드 수정좀 해줘여

--- DB/test.py
+++ DB/test.py
@@ -4,51 +4,32 @@ from rich.console import Console
 from rich.table import Table
 
 # settings
-db = client["school"]
-students = db["students"]
+db = client["jihoo"]
+players = db["hangman-players"]
 console = Console()
 
+def sign_up(nick_name, password):
+    if players.find({
 
-# function
-def find_grade(grade):
-    return list(students.find({"grade": grade}))
+        'nickname' : nick_name 
 
+    }):
+        print('이미 존재함 ㅅㄱ')
 
-def print_grade_table(grade, data):
-    table = Table(
-        title=f"Students with Grade {grade}",
-        title_style="bold magenta",
-        header_style="bold cyan",
-        border_style="bright_blue"
-    )
+    players.insert_one({
 
-    # 컬럼 구조 — 학생 데이터 구조에 맞게 컬럼을 원하는대로 더 추가 가능
-    table.add_column("Name", justify="left")
-    table.add_column("Age", justify="center")
-    table.add_column("Major", justify="left")
-    table.add_column("Grade", justify="center")
+        'nickname' : nick_name,
+        'password' :  password 
 
-    if not data:
-        table.add_row("-", "-", "-", f"{grade} (No Data)")
-    else:
-        for s in data:
-            table.add_row(
-                str(s.get("name", "-")),
-                str(s.get("age", "-")),
-                str(s.get("major", "-")),
-                str(s.get("grade", "-"))
-            )
-
-    console.print(table)
-    console.print("\n")  # 줄 간격
-
+    })
 
 # main
-grades = ["A", "B", "C", "D", "E", "F"]
 
-for g in grades:
-    result = find_grade(g)
-    print_grade_table(g, result)
+nickname = input()
+password = input()
+
+sign_up(nickname, password)
+
 
 
 
--- assm/hackman/setting.py
+++ assm/hackman/setting.py
@@ -1,4 +1,12 @@
+import DB_module
+
+client = DB_module.client
+db = client['jihoo']
+players = db['hangman-players']
+
 LIFE = 10
 WORD_LIST = ['apple', 'april', 'banana', 'blue', 'coral', 'dictionary', 'flower', 'peach',
 'strawberry', 'watermelon']
 USED = []
+
+
