9faf788ba642853d97e69d2d0a5303a3a22bb75c
yojun313 <moonyojun@gmail.com>
2026-01-06 18:55:10 +0900

    Refactor NPC initialization and enhance add_npc method for better data handling

--- assm/gasstation/npc.py
+++ assm/gasstation/npc.py
@@ -8,35 +8,54 @@ import module
 class Npc:
     def __init__(self):
         if setting.npc.count_documents() < 0:
-            self.add_npc()
+            data = self.add_npc()
         else:
-            make_new = module.chance(70)
-            if make_new:
-                self.add_npc()
+            if module.chance(70):
+                data = self.add_npc()
             else:
-                selected_npc = random.choice(list(setting.npc.find()))
-                
-
-    def add_npc():
-        setting.npc.insert_one({
+                data = random.choice(list(setting.npc.find()))
+        
+        self.id           = data['id']
+        self.name         = data['name']
+        self.talk_history = data['talk_history']
+        self.likeablity   = data['likeability']
+        self.visit_cnt    = data['visit_cnt']
+        
+    def add_npc(self):
+        data = {
             'id' : str(uuid.uuid4()),
-            'name' : str(llm.answer('npc에게 사용될 수있는 랜덤한 이름을 만들어줘. 이름만 답해.')),
+            'name' : str(llm.llm_answer('npc에게 사용될 수있는 랜덤한 이름을 만들어줘. 이름만 답해.')),
             'talk_history' : [],
             'likeability' : 0, #ai한테 대화 바탕 호감도 측정
             'visit_cnt' : 0
+        }
+        setting.npc.insert_one(data)
+        return data
+    
+    def update_npc(self):
+        data = {
+            'id': self.id,
+            'name': self.name,
+            'talk_history': self.talk_history,
+            'likeability': self.likeablity,
+            'visit_cnt': self.visit_cnt
+        }
+        setting.npc.update_one({
+            'id': self.id
+        }, {
+            '$set': data
         })
 
-
-    def normal_response_before_refill(self):
-        pass
-
-    def normal_response_after_refill(self, reply):
-        pass
-    
-    def abnormal_response_before_refill(self, reply):
-        pass
-
-    def abnormal_response_after_refill(self, reply):
-        pass
-
-
+    def chatbot(self):
+        print("챗봇 시작")
+        
+        while True:
+            query = str(self.talk_history) + input("입력: ")
+            answer = llm.llm_answer(query)
+            
+            history = {
+                'query': query,
+                'answer': answer
+            }
+            self.talk_history.append(history)
+            self.update_npc()
