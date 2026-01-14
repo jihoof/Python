709fcb1adac9010b4b4ebff283b97bee8dd4022d
yojun313 <moonyojun@gmail.com>
2026-01-14 21:41:03 +0900

    Refactor sign_up and sign_in functions for improved validation and password handling; update DB client initialization

--- assm/gasstation/auth.py
+++ assm/gasstation/auth.py
@@ -1,40 +1,57 @@
-import DB_module
 import module
 import setting
 import getpass
+import hashlib
+import os
 
 def sign_up():
-    nickname = input('아이디를 입력하세요: ')
-    password = getpass.getpass('비밀번호 입력하세요: ')
+    while True:
+        nickname = input('아이디를 입력하세요: ')
+        if setting.players.find_one({'nickname' : nickname }):
+            print('이미 존재하는 아이디 입니다.')
+            continue
+        
+        password = getpass.getpass('비밀번호 입력하세요: ')
+        re_password = getpass.getpass('비밀번호를 다시 입력하세요: ')
 
-    if setting.players.find_one({
-        'nickname' : nickname 
-    }):
-        print('이미 존재하는 아이디 입니다.')
-    else:
-        setting.players.insert_one({
-            'nickname' : nickname,
-            'password' :  password, 
-            'custom' : [],
-            'cleared' : [],
-            'live_status' : {
-                "left_life" : 0, "current_used" : [], "current_word" : 'None', "playing" : False
-            },
-            'ai_list' : [],
-            'bought_dlc' : [],
-            'coin' : 0
-        })
-        print('정상적으로 계정이 생성되었습니다.')
-        module.enter()
+        if password != re_password:
+            print("재입력한 비밀번호가 올바르지 않습니다")
+            continue
+            
+        break
+    
+    salt = os.urandom(16)
+    encrypted_password = hash_password(password, salt)
+
+    setting.players.insert_one({
+        'nickname' : nickname,
+        'password' :  encrypted_password, 
+        'salt': salt,
+    })
+    print('정상적으로 계정이 생성되었습니다.')
+    module.enter()
 
 def sign_in():
-    nickname = input('닉네임을 입력하세요: ')
-    password = getpass.getpass('비밀번호를 입력하세요: ')
+    while True:
+        nickname = input('닉네임을 입력하세요: ')
+        password = getpass.getpass('비밀번호를 입력하세요: ')
 
-    if setting.players.find_one({
-        "nickname" : nickname,
-        "password" : password
-    }):
-        return nickname
-    else:
-        return False
+        info = setting.players.find_one({"nickname" : nickname})
+        if not info:
+            print("유저가 존재하지 않습니다")
+            continue
+            
+        salt = info['salt']
+        encrypted_password = hash_password(password, salt)
+        
+        if info['password'] != encrypted_password:
+            print("비밀번호가 올바르지 않습니다")
+            continue
+        
+        print("로그인 되었습니다")
+        return info
+    
+def hash_password(password, salt):
+    return hashlib.sha256(salt + password.encode()).hexdigest()
+
+sign_in()
--- assm/gasstation/setting.py
+++ assm/gasstation/setting.py
@@ -1,6 +1,6 @@
 import DB_module
 
-client = DB_module.client()
+client = DB_module.client
 db = client['jihoo']
 players = db['gasstation-players']
 npc = db['gasstation-npc']
